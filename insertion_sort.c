af5ee5dc3b439b15be9127affb056e975b9551bf
GurjeetSandhu <sandhugurjeet143@gmail.com>
2020-08-22 15:45:48 +0530

    comment

--- insertion_sort.c
+++ insertion_sort.c
@@ -5,19 +5,30 @@
 void insertion(int array[], int m)
 {
   int j,k,temp;
-  for(j=1;j>m;j++)
+  for(j=1;j<m;j++)
     {
       temp = array[j];
       k=j-1;
-      while(j>=0 && array[k]>temp)
+      while(k>=0 && array[j]>temp)
 	{
 	  array[k+1]=array[k];
-	  k--;
+	  k=k-1;
 	}
       array[k+1]=temp;
          }
   
 }
+
+void printarr(int array[], int g)
+
+{ int i;
+    printf("\n Sorted elements are");
+    	for(i=0;i<g;i++)
+	  {
+	    printf("\n %d \n ", array[i]);
+      	  } 
+}
+
 
 int main()
 {
@@ -32,10 +43,6 @@ int main()
 	scanf("%d",& a[i]);
 	      }
     insertion(a, n);
-	  printf("\n Sorted elements are");
-	for(i=0;i<n;i++)
-	  {
-	    printf("\n %d \n ", a[i]);
-      	  }
+    printarr(a, n);
 	return 0;
       }
